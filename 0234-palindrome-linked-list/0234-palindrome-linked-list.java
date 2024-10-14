b0b09a17b5dec7dc92f8ce1dbb1b2757f8446ebd
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2024-10-15 00:36:56 +0530

    Time: 3 ms (99.68%), Space: 69.4 MB (8.84%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,30 +1,45 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
+        ListNode h1 = head;
+        int count = 0;
+        while (h1 != null) { // counting nodes
+            h1 = h1.next;
+            count++;
         }
-        int left = 0;
-        int right = list.size() - 1;
-        while (left < right) {
-            if (list.get(left) != list.get(right)) {
+        ListNode left = null;
+        ListNode right = null;
+        ListNode prev = null;
+        // splitting by the middle and reversing first half
+        for (int i = 1; i <= count / 2; i++) {
+            right = head.next;
+            head.next = prev;
+            prev = head;
+            left = head;
+            head = right;
+            if (i == count / 2 && count % 2 == 1) { // handle right half start for odd node count
+                right = right.next;
+            }
+        }
+
+        // comparing to half
+        while (left != null) {
+            if (left.val != right.val) { // if value not equal -> return false
                 return false;
             }
-            left++;
-            right--;
+            left = left.next;
+            right = right.next;
         }
-        return true;
 
+        return true; // return true if all values was equal
     }
 }
