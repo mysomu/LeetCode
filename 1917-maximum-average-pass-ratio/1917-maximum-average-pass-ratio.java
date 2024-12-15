4583f564e0218d326ca0198c44a0fd196458f1fa
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2024-12-16 00:42:46 +0530

    Time: 369 ms (56.96%), Space: 95.5 MB (70.73%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,35 +1,34 @@
+import java.util.PriorityQueue;
+
 class Solution {
-    public static double nextPassRatioDiff(double pass, double total){
-        return (pass + 1) / (total + 1) - (pass / total);
-    }
-
-
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        int n = classes.length;
-        PriorityQueue<double[] > maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
-        //entry = {nextPassRatioDiff, pass, total}
-        for(int[] arr: classes){
-            int pass = arr[0];
-            int total = arr[1];
-            double next = nextPassRatioDiff(pass, total);
-            maxHeap.offer(new double[]{next, pass, total});
+        PriorityQueue<double[]> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int[] cls : classes) {
+            int pass = cls[0], total = cls[1];
+            double diff = (double) (pass + 1) / (total + 1) - (double) pass / total;
+            maxHeap.offer(new double[]{diff, pass, total});
         }
-        while(extraStudents > 0){
-            double[] top= maxHeap.poll();
-            double pass = top[1] + 1;
-            double total = top[2] + 1;
-            double next = nextPassRatioDiff(pass, total);
-            maxHeap.offer(new double[]{next, pass, total});
+
+        while (extraStudents > 0) {
+            double[] top = maxHeap.poll();
+            double diff = top[0];
+            int pass = (int) top[1];
+            int total = (int) top[2];
+            
+            pass++;
+            total++;
+            diff = (double) (pass + 1) / (total + 1) - (double) pass / total;
+
+            maxHeap.offer(new double[]{diff, pass, total});
             extraStudents--;
         }
-        double totalPassRatio = 0;
-        while(!maxHeap.isEmpty()){
-            double[] arr = maxHeap.poll();
-            double pass = arr[1];
-            double total = arr[2];
-            totalPassRatio += (pass / total);
-        }
-        return totalPassRatio / n;
-    }
 
+        double ans = 0;
+        while (!maxHeap.isEmpty()) {
+            double[] top = maxHeap.poll();
+            ans += (double) top[1] / top[2];
+        }
+        return ans / classes.length;
+    }
 }
