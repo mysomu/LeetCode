ac915b3310a3aaf1e238ab6f5bdc254011941be9
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2025-03-15 15:24:33 +0530

    Time: 23 ms (65.42%), Space: 60.9 MB (40.42%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,44 +1,35 @@
 class Solution {
+    bool isPossible(vector<int> &arr, int k, int maxi) {
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] <= maxi) {
+                i += 2;
+                k--;
+            } else {
+                i++;
+            }
+
+            if(k == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 public:
-    int helper(int i,vector<int> &nums,int x,vector<int> &dp){
-        if(i < 0){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int include, exclude;
-        include = exclude = 0;
-        if(nums[i] <= x){
-            include = 1 + helper( i-2, nums, x, dp);
-        }
-        exclude = helper( i-1, nums, x, dp);
-        return dp[i] = max( include, exclude);
-    }
-    bool predicate(vector<int> &nums,int k,int x){
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        // dp[i] : maximum number of non adjacent houses that can be robbed from nums(0...i) 
-        // and each robbed houses must be lesser than equal to x
-        helper( n-1, nums, x, dp);
-        return dp[n-1] >= k;
-    }
-    int minCapability(vector<int>& nums, int k) {
-        int l = INT_MAX;
-        int r = INT_MIN;
-        for(int num : nums){
-            l = min( l, num);
-            r = max( r, num);
-        }
-        int m;
+    int minCapability(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = *min_element(arr.begin(), arr.end());
+        int high = *max_element(arr.begin(), arr.end());
+
         int ans = -1;
-        while(l <= r){
-            m = l + (r-l)/2;
-            if(predicate( nums, k, m)){
-                ans = m;
-                r = m-1;
-            }else{
-                l = m+1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(isPossible(arr, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
