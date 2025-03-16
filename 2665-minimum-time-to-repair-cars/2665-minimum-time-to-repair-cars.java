5477a27ea829eacfb0f54302266814e3af530f97
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2025-03-16 10:51:38 +0530

    Time: 38 ms (65.86%), Space: 56.3 MB (51.08%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -1,25 +1,24 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
-        long left = 1, right = (long) Arrays.stream(ranks).min().getAsInt() * cars * cars;
-
-        while (left < right) {
-            long mid = (left + right) / 2;
-            if (canRepairAll(ranks, cars, mid)) {
-                right = mid;
-            } else {
+        long left = 1;
+        long right = (long) ranks[0] * cars * cars;
+        while(left < right)
+        {
+            long mid = (left + right)/2;
+            long currCars = 0;
+            for(int rank : ranks)
+            {
+                currCars += (long) Math.sqrt(mid/rank);
+            }
+            if(currCars < cars)
+            {
                 left = mid + 1;
             }
+            else
+            {
+                right = mid;
+            }
         }
-
         return left;
-    }
-
-    private boolean canRepairAll(int[] ranks, int cars, long time) {
-        long totalCarsRepaired = 0;
-        for (int rank : ranks) {
-            totalCarsRepaired += Math.sqrt(time / rank);
-            if (totalCarsRepaired >= cars) return true;
-        }
-        return false;
     }
 }
