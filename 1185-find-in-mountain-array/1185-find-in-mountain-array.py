68378a387f957182f0243ac2c36301ad7fdbcf48
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2025-03-26 19:52:53 +0530

    Time: 35 ms (78.5%), Space: 18.5 MB (57.59%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.py
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.py
@@ -7,36 +7,35 @@
 #    def length(self) -> int:
 
 class Solution:
-    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        leng = mountain_arr.length() 
-        left, right = 0, leng-1
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        def binary_search(l, r, key, asc=True):
+            while l <= r:
+                mid = (l + r) // 2
+                val = mountainArr.get(mid)
+                if val == key:
+                    return mid
+                if (val < key) == asc:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return -1
+
+        n = mountainArr.length()
+        
+        # Find peak element
+        left, right = 0, n - 1
         while left < right:
-            mid = (left+right) // 2
-            if mountain_arr.get(mid) >= mountain_arr.get(mid+1):
-                right = mid
+            mid = (left + right) // 2
+            if mountainArr.get(mid) < mountainArr.get(mid + 1):
+                left = mid + 1
             else:
-                left = mid + 1 
+                right = mid
         peak = left
 
-        l, r = 0, peak
-        while l <= r: 
-            mid = (l + r) // 2
-            cur = mountain_arr.get(mid)
-            if cur == target:
-                return mid
-            elif cur > target:
-                r = mid - 1
-            else:
-                l = mid + 1
-                
-        l, r = peak+1, leng-1
-        while l <= r:
-            mid = (l + r) // 2
-            cur = mountain_arr.get(mid)
-            if cur == target:
-                return mid
-            elif cur > target:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return -1
+        # Search in the left (ascending) part
+        res = binary_search(0, peak, target, True)
+        if res != -1:
+            return res
+
+        # Search in the right (descending) part
+        return binary_search(peak + 1, n - 1, target, False)
