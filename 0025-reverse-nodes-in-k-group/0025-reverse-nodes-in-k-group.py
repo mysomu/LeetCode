0cb865037e11afbd328419cd3fcfb19e5d3691fe
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2025-03-30 22:48:00 +0530

    Time: 3 ms (32.11%), Space: 18.7 MB (19.58%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,33 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        pointer = dummy
+        count, temp = 0, head
         
-        while pointer:
-            node = pointer
-            for i in range(k):
-                if not node.next:
-                    return dummy.next
-                node = node.next
-            
-            prev, curr = None, pointer.next
-            for i in range(k):
-                next_node = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_node
-            
-            tail = pointer.next
-            tail.next = curr
-            pointer.next = prev
-            pointer = tail
+        # Check if there are at least k nodes to reverse
+        while temp and count < k:
+            temp = temp.next
+            count += 1
         
-        return dummy.next
+        if count < k:
+            return head  # Not enough nodes to reverse, return head as is
+        
+        # Reverse k nodes
+        new_head = self.reverse(head, temp)
+        
+        # Recursively reverse the next part
+        head.next = self.reverseKGroup(temp, k)
+        
+        return new_head
 
-    
+    def reverse(self, start: ListNode, end: ListNode) -> ListNode:
+        prev, curr = None, start
+        while curr != end:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
+
