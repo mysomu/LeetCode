34fc2c95df3685c36ec092ba6a086293426fb7f7
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2025-04-06 00:45:57 +0530

    Time: 175 ms (95.01%), Space: 75.6 MB (85.71%) - LeetHub

--- 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,23 +1,30 @@
+import math
+from typing import List
+
+
 class Solution:
-  def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
-    ans = 0
-    graph = [[] for _ in range(len(roads) + 1)]
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        n = len(roads) + 1
+        adj = [[] for _ in range(n)]
 
-    for u, v in roads:
-      graph[u].append(v)
-      graph[v].append(u)
+        for u, v in roads:
+            adj[u].append(v)
+            adj[v].append(u)
 
-    def dfs(u: int, prev: int) -> int:
-      nonlocal ans
-      people = 1
-      for v in graph[u]:
-        if v == prev:
-          continue
-        people += dfs(v, u)
-      if u > 0:
-        # # of cars needed.
-        ans += int(math.ceil(people / seats))
-      return people
+        total_fuel = 0
 
-    dfs(0, -1)
-    return ans
+        def dfs(node: int, parent: int) -> int:
+            nonlocal total_fuel
+            people = 1
+
+            for child in adj[node]:
+                if child != parent:
+                    people += dfs(child, node)
+
+            if node != 0:
+                total_fuel += math.ceil(people / seats)
+
+            return people
+
+        dfs(0, -1)
+        return total_fuel
