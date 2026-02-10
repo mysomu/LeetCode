f60767caf9b63bdbbf6bf53082ca466f9c2365d9
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2026-02-10 11:56:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (25.24%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -4,18 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-  def rotateRight(self, head: ListNode, k: int) -> ListNode:
-    if not head or not head.next or k == 0:
-      return head
-    tail = head
-    length = 1
-    while tail.next:
-      tail = tail.next
-      length += 1
-    tail.next = head  
-    t = length - k % length
-    for _ in range(t):
-      tail = tail.next
-    newHead = tail.next
-    tail.next = None
-    return newHead        
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+
+        # Step 1: find length and tail
+        tail = head
+        length = 1
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        # Step 2: optimize k
+        k = k % length
+        if k == 0:
+            return head
+
+        # Step 3: make list circular
+        tail.next = head
+
+        # Step 4: find new tail
+        rotate = length - k
+        curr = head
+        for _ in range(1,rotate):
+            curr = curr.next
+
+        # Step 5: break the circle
+        newHead = curr.next
+        curr.next = None
+
+        return newHead
