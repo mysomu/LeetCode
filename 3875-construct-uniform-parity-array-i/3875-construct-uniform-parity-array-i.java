35e494b2a695e1f6769846bb2f533c734a35d789
SOUMEN GHOSH <96889167+mysomu@users.noreply.github.com>
2026-09-02 21:28:41 +0530

    Time: 1 ms (31.89%), Space: 45 MB (87.6%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        int n = nums1.length;
+        int [] nums2 = new int[n];
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums2[i]==nums1[i] && nums2[j]!=nums1[j]){
+                    return true;
+                }else{
+                    if(nums2[i]==nums1[i] - nums1[j]){
+                        return true;
+                    }
+                }
+            }
+        }
         return true;
     }
 }
